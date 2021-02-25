195670f495333e7e59d0467be6c1546dad52ce7a
Dieter97 <dieter.balemans@hotmail.com>
2021-02-25 20:13:01 +0100

    update cars

--- streets/car.py
+++ streets/car.py
@@ -2,6 +2,6 @@
 
 class Car:
 
-    def __init__(self, n_streets, streets):
+    def __init__(self, n_streets):
         self.n_streets = n_streets
-        self.streets = streets
+        self.streets = []
