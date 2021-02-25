73471cfdc7a665365f36ea7cf070ae00c8f0a0cc
reinout <BeepBoop>
2021-02-25 20:36:29 +0100

    Cleanup

--- pizza/pizza.py
+++
@@ -1,9 +0,0 @@
-
-
-class Pizza:
-
-    def __init__(self, index, n_in, ingeredients):
-        super().__init__()
-        self.index = index
-        self.n_in = n_in
-        self.ingredient_list = set(ingeredients)
