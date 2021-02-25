37220b76c6753b7c19f5c34995c89414db0585b7
Niels Balemans <balemans.niels@gmail.com>
2021-02-25 20:54:43 +0100

    added get_busiest_street to intersection

--- streets/intersection.py
+++ streets/intersection.py
@@ -14,6 +14,13 @@ class Intersection:
         self.output_streets: List[Street] = []
         self.schedule = []
 
+    def get_busiest_street(self) -> Street:
+        busiest = self.input_streets[0]
+        for i in range(1, len(self.input_streets)):
+            if len(busiest.cars) > len(self.input_streets[i].cars):
+                busiest = self.input_streets[i]
+        return busiest
+
     def is_busy(self) -> bool:
         for street in self.input_streets:
             if street.cars.empty():
@@ -34,6 +41,6 @@ class Intersection:
     # TODO verwijderen, moet dict worden
     def get_car_street(self, car) -> Street:
         for street in self.output_streets:
-            if street.name == car.streets[car.current_street_index + 1].name:
+            if street.name == car.streets[car.current_street_index + 1]:
                 return street
         raise Exception
